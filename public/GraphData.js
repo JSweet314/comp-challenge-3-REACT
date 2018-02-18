ebe11658006a3c269a88c263ccef84260abc1b15
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-02-17 23:11:20 -0700

    Create data for graphs

---
+++ public/GraphData.js
@@ -0,0 +1,29 @@
+const GraphData = [{
+  label: "APPLICANTS",
+  number: "3,154",
+  strokeDasharray: "79 21",
+  baseColor: "#d2d3d4",
+  graphColor: "#66B9BE",
+  circumference: 100,
+  className: "circle-graph circle-graph--left"
+},
+{
+  label: "INTERVIEWS",
+  number: "1,546",
+  strokeDasharray: "39 61",
+  baseColor: "#d2d3d4",
+  graphColor: "#3580B7",
+  circumference: 100,
+  className: "circle-graph"
+},
+{
+  label: "FORWARDS",
+  number: "912",
+  strokeDasharray: "19 81",
+  baseColor: "#d2d3d4",
+  graphColor: "#A26DAB",
+  circumference: 100,
+  className: "circle-graph circle-graph--right"
+}];
+
+module.exports = GraphData;
